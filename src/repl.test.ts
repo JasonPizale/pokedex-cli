26b2e6d7d8c8d2cca4214b05d6333d7a503fbf8c
Jason Pizale <jasonpizale@gmail.com>
2026-04-30 14:57:22 -0400

    initial project file setup

---
+++ src/repl.test.ts
@@ -0,0 +1,35 @@
+import { cleanInput } from "./repl.js";
+import { describe, expect, test } from "vitest";
+
+describe.each([
+  {
+    input: "  hello  world  ",
+    expected: ["hello", "world"],
+  },
+  {
+    input: "Charmander Bulbasaur PIKACHU",
+    expected: ["charmander", "bulbasaur", "pikachu"],
+  },
+  {
+    input: "   one   two   three   ",
+    expected: ["one", "two", "three"],
+  },
+  {
+    input: "",
+    expected: [],
+  },
+  {
+    input: "   ",
+    expected: [],
+  },
+])("cleanInput($input)", ({ input, expected }) => {
+  test(`Expected: ${expected}`, () => {
+    const actual = cleanInput(input);
+
+    expect(actual).toHaveLength(expected.length);
+
+    for (const i in expected) {
+      expect(actual[i]).toBe(expected[i]);
+    }
+  });
+});
