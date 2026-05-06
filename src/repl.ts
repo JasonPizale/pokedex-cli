0f5d63d532001f2a9881ed711643cfea4d456c42
JasonPizale <jasonpizale@gmail.com>
2026-05-06 15:11:43 -0400

    Add help and exit commands to REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,5 @@
 import { createInterface } from "readline";
+import { getCommands } from "./commands.js";
 
 export function startREPL() {
   const rl = createInterface({
@@ -11,13 +12,28 @@ export function startREPL() {
 
   rl.on("line", async (input) => {
     const words = cleanInput(input);
+
     if (words.length === 0) {
       rl.prompt();
       return;
     }
 
     const commandName = words[0];
-    console.log(`Your command was: ${commandName}`);
+    const commands = getCommands();
+    const command = commands[commandName];
+
+    if (!command) {
+      console.log(`Unknown command`);
+      rl.prompt();
+      return;
+    }
+
+    try {
+      command.callback(commands);
+    } catch (err) {
+      console.log(err);
+    }
+
     rl.prompt();
   });
 }
